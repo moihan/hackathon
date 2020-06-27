b90f99833425c370b5842f21a88ac8c38add8dc7
ELCOT <ELCOT@Lenovo>
2020-06-27 11:25:56 +0530

    First commit message

---
+++ cabBooking/src/main/java/team1/cabBooking/reportmanager.java
@@ -0,0 +1,29 @@
+package team1.cabBooking;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.aventstack.extentreports.reporter.configuration.ChartLocation;
+
+public class reportmanager {
+	//public static ExtentHtmlReporter htmlReporter;
+	public static ExtentReports report;
+	
+public static ExtentReports getReportInstance(){
+	
+	if(report==null){
+		ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir")+"//target//ttt.html");
+	report =new ExtentReports();
+	report.attachReporter(htmlReporter);
+	report.setSystemInfo("OS","Windows 10");
+	report.setSystemInfo("Environment","UAT");
+	report.setSystemInfo("Build Number","10.8.1");
+	report.setSystemInfo("Browser","Chrome");
+	htmlReporter.config().setDocumentTitle("UAT Automation Result");
+	htmlReporter.config().setReportName("All headlines");
+	htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
+	htmlReporter.config().setTimeStampFormat("MM dd,yyyy HH:mm:ss");
+	}
+	return report;
+}
+	
+}
